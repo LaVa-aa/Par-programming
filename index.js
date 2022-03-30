7d5efbe66bd88da303d8541843a4de27d0670fe2
Lava Maamo <lama26830@edu.ucl.dk>
2022-03-30 10:17:38 +0200

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+const baseUrl="https://restduathlon.azurewebsites.net/api/Duathletes"
+
+Vue.createApp({
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        
+    }
+}).mount("#app")
