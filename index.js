1ba47ca842297520f088d068dc6a1b9b90ff9c8a
Lava Maamo <lama26830@edu.ucl.dk>
2022-03-30 10:58:33 +0200

    get done

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const baseUrl="https://restduathlon.azurewebsites.net/api/Duathletes"
 
-Vue.createApp({
+const app = Vue.createApp({
     data(){
         return{
+            //properties
+            duathletes:[],//array 
+            error:null, //ingen ting ikke nogle objekt
 
         }
     },
     methods:{
-        
+        getAllDuathletes(){
+            this.getAllDuathleteshelper(baseUrl)
+        },
+       async getAllDuathleteshelper(url){
+            try {//fejl håndtering 
+                const result = await axios.get(url)
+                this.duathletes= result.data
+                console.log(this.duathletes)
+                //console.writeline udskriver nogle
+            } catch (ex) {//exception
+                alert(ex.message) 
+            }
+        },
+
     }
 }).mount("#app")
