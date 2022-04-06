f2088c1522739a6251caac45e9b63b79bc542f8e
Lava Maamo <lama26830@edu.ucl.dk>
2022-04-06 09:54:09 +0200

    del post

--- index.js
+++ index.js
@@ -23,6 +23,28 @@ const app = Vue.createApp({
                 alert(ex.message) 
             }
         },
+        async postDuathletes(){
+            try {
+                const result = await axios.post(baseUrl, this.postData)
+                this.duathleteMessage ="Response: " + result.status + " " + result.statusText
+                //efter den har postet, kaldes metoden get, så listen opdateres med det nye
+                this.getAllDuathletes()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async deleteDuathletes(deleteBib){
+            const url = baseUrl + "/" + deleteBib
+            try {
+                 const result = await axios.delete(url)
+                 this.duathleteMessage = result.status + " " + result.statusText
+                 this.getAllDuathletes()
+            }catch(ex){
+                //message will be shown if error
+                alert(ex.message)
+            }
+        },
+        
 
     }
 }).mount("#app")
