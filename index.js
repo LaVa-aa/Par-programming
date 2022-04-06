08a401cf7a0c018ca3a9069756216aef44d14078
DreamMln <maix2374@edu.zealand.dk>
2022-04-06 10:20:12 +0200

    u

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             //properties
             duathletes:[],//array 
             error:null, //ingen ting ikke nogle objekt
-
+            postData: {
+                name: "",
+                ageGroup: 0,
+                bike: 0,
+                run: 0 
+            },
+            duathleteMessage: "",
         }
     },
     methods:{
@@ -23,6 +29,26 @@ const app = Vue.createApp({
                 alert(ex.message) 
             }
         },
-
+        async postDuathletes(){
+            try {
+                const result = await axios.post(baseUrl, this.postData)
+                this.duathleteMessage ="Response: " + result.status + " " + result.statusText
+                //efter den har postet, kaldes metoden get, så listen opdateres med det nye
+                this.getAllDuathletes()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async deleteDuathletes(deleteBib){
+            const url = baseUrl + "/" + deleteBib
+            try {
+                 const result = await axios.delete(url)
+                 this.duathleteMessage = result.status + " " + result.statusText
+                 this.getAllDuathletes()
+            }catch(ex){
+                //message will be shown if error
+                alert(ex.message)
+            }
+        },
     }
 }).mount("#app")
